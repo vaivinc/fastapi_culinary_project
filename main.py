9f698ff1ed751e842f157805d2a11c4a3d265e86
vaivinc <168576062+vaivinc@users.noreply.github.com>
2025-01-14 01:11:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-from routes import user_route, auth_route
+from routes import user_route, auth_route, receipe_route
 app = FastAPI(docs_url="/docs")
 
 
@@ -13,6 +13,7 @@ async def root():
 
 app.include_router(user_route, prefix="/account", tags=["users"])
 app.include_router(auth_route, prefix="/auth", tags=["auth"])
+app.include_router(receipe_route, prefix="/receipe", tags=["receipe"])
 
 if __name__ == "__main__":
     uvicorn.run(f"{__name__}:app", reload=True)
